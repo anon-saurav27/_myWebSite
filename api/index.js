9e1a8bf5dd873da533d2ccfdf1cdcf0894dc7bb0
Saurav Giri <saurav130giri@gmail.com>
2025-01-09 21:56:25 +0545

    create models and userSchema

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(300,()=>{
-    console.log("server is running in port 3000");
+app.listen(300, () => {
+  console.log("server is running in port 3000");
 });
