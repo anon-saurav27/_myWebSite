6c030aff3e717576a80cba1d7646c57207d8c9ba
Saurav Giri <saurav130giri@gmail.com>
2025-01-09 22:54:48 +0545

    created hashpassword

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -16,11 +17,14 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 const port=process.env.BACKEND_PORT;
 app.listen(port, () => {
   console.log(`server is running in port ${port}`);
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 
