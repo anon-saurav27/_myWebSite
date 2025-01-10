9130d45acd9ad5ed3dabd38efa4e0bba6b2a2c61
Saurav Giri <saurav130giri@gmail.com>
2025-01-10 20:09:30 +0545

    created theme dark and light

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,8 +3,12 @@ import userReducer  from './user/userSlice.js'
 import { persistReducer } from 'redux-persist'
 import persistStore from 'redux-persist/es/persistStore'
 import storage from 'redux-persist/lib/storage'
+import themeReducer from './theme/theme.slice.js'
+
+
 const rootReducer = combineReducers({
     user: userReducer,
+    theme: themeReducer,
 })
 const persistConfig={
     key: 'root',
